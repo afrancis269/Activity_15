22c777e691e2afd734c729115f70813b21ba8f18
th4nhf <afrancis269@gmail.com>
2022-02-13 17:01:16 +0700

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MonthCbx.DataSource = CultureInfo.InvariantCulture.DateTimeFormat
+                                                     .MonthNames.Take(12).ToList();
+            MonthCbx.SelectedItem = CultureInfo.InvariantCulture.DateTimeFormat
+                                                    .MonthNames[DateTime.Now.AddMonths(-1).Month - 1];
+
+            YearCbx.DataSource = Enumerable.Range(1983, DateTime.Now.Year - 1983 + 1).ToList();
+            YearCbx.SelectedItem = DateTime.Now.Year;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int month = MonthCbx.SelectedIndex + 1;
+            int year = DateTime.Now.Year;
+            if (YearCbx.SelectedIndex != -1)
+            {
+                year = Int32.Parse(YearCbx.Items[YearCbx.SelectedIndex].ToString());
+            }
+            int num = year - month * (DayCbx.SelectedIndex + 1);
+            Form2 frm2 = new Form2(num);
+            frm2.Show();
+
+        }
+
+        private void SetDates()
+        {
+            DayCbx.Items.Clear();
+            int month = MonthCbx.SelectedIndex + 1;
+            int year = DateTime.Now.Year;
+            if (YearCbx.SelectedIndex != -1)
+            {
+                year = Int32.Parse(YearCbx.Items[YearCbx.SelectedIndex].ToString());
+            }
+
+            for (int i = 1; i <= DateTime.DaysInMonth(year, month); i++)
+            {
+                DayCbx.Items.Add(i);
+            }
+        }
+
+        private void MonthCbx_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SetDates();
+            
+        }
+
+        private void YearCbx_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SetDates();
+        }
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Activity_15
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumberTxb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NumberTxb
+            // 
+            this.NumberTxb.Location = new System.Drawing.Point(66, 68);
+            this.NumberTxb.Name = "NumberTxb";
+            this.NumberTxb.Size = new System.Drawing.Size(100, 22);
+            this.NumberTxb.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your lucky number is";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 134);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NumberTxb);
+            this.Name = "Form2";
+            this.Text = "Your Result!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NumberTxb;
+        private System.Windows.Forms.Label label1;
+    }
+}
